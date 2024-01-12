9dfa400da830aa5e5173ab2f0a0ea70b6372dad6
Juanma <juanmartin.pilon@gmail.com>
2024-01-12 17:04:25 -0300

    finished :D

--- Palindrome Checker/script.js
+++ Palindrome Checker/script.js
@@ -1,24 +1,26 @@
 const input = document.getElementById("check-btn");
-const inputText = document.getElementById("text-input")
+const inputText = document.getElementById("text-input");
+const resultValue = document.getElementById("result");
 
-input.addEventListener('click', (e) => {
-    const inputValue = inputText.value.trim();
-    console.log(inputValue);
-    if(inputValue === ""){
-        alert("Please input a value");
-    } else {
-        checkPalindromo(inputValue);
-    }
-   
+input.addEventListener("click", (e) => {
+  const inputValue = inputText.value.trim();
+  console.log(inputValue);
+  if (inputValue === "") {
+    alert("Please input a value");
+  } else {
+    checkPalindromo(inputValue);
+  }
 });
 
 function checkPalindromo(inputValue) {
-    let palabra = inputValue.split('');
-    palabra.reverse();
-    palabra = palabra.join('');
-    if (palabra === inputValue) {
-        return alert("es pali");
-    } else {
-        return alert("no es pali");
-    }
+  let palabraLimpia = inputValue.replace(/[^a-zA-Z0-9]/g, "");
+  let palabra = palabraLimpia.split("");
+  palabra.reverse();
+  palabra = palabra.join("");
+  console.log(palabra);
+  if (palabra.toLowerCase() === palabraLimpia.toLowerCase()) {
+    return (resultValue.innerText = `${inputValue} is a palindrome`);
+  } else {
+    return (resultValue.innerText = `${inputValue} is not a palindrome`);
+  }
 }
