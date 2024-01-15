5d29cbe5bfd770e25e3f54e76a2ed74162129446
Juanma <juanmartin.pilon@gmail.com>
2024-01-15 14:38:02 -0300

    update

--- Football Team Cards/script.js
+++ Football Team Cards/script.js
@@ -180,4 +180,20 @@ teamName.textContent = team;
 worldCupYear.textContent = year;
 headCoach.textContent = coachName;
 
-const setPlayerCards = () => {};
+const setPlayerCards = (arr = players) => {
+
+
+  playerCards.innerHTML += arr.map(
+    ({ name, position, number, isCaptain, nickname }) => {
+      `
+        <div class="player-card">
+          <h2>${name} ${isCaptain ? "(Captain)" : ""}</h2>
+          <p>Position: ${position}</p>
+          <p>Number: ${number}</p>
+        </div>
+      `;
+    }
+  );
+
+
+};
