2645618dc529134d4772723946e6c687f7d49ee7
Juanma <juanmartin.pilon@gmail.com>
2024-01-26 23:09:05 -0300

    finishing this one

--- Roman to Numeral/script.js
+++ Roman to Numeral/script.js
@@ -2,43 +2,47 @@ const convertBtn = document.getElementById('convert-btn');
 const getInput = document.getElementById('number');
 const getOutput = document.getElementById('output');
 
-const numCheck = () => {
-    if (getInput.value === '') {
-        getOutput.innerText = "Please enter a valid number";
-    } else if (getInput.value < 0) {
-        getOutput.innerText = "Please enter a number greater than or equal to 1";
-    } else if (getInput.value > 3999) {
-        getOutput.innerText = "Please enter a number less than or equal to 3999";
+const numToRoman = (num) => {
+    const romanNumerals = [
+        { value: 1000, numeral: 'M' },
+        { value: 900, numeral: 'CM' },
+        { value: 500, numeral: 'D' },
+        { value: 400, numeral: 'CD' },
+        { value: 100, numeral: 'C' },
+        { value: 90, numeral: 'XC' },
+        { value: 50, numeral: 'L' },
+        { value: 40, numeral: 'XL' },
+        { value: 10, numeral: 'X' },
+        { value: 9, numeral: 'IX' },
+        { value: 5, numeral: 'V' },
+        { value: 4, numeral: 'IV' },
+        { value: 1, numeral: 'I' }
+    ];
+
+    let res = '';
+    
+    for (let i = 0; i < romanNumerals.length; i++) {
+        while (num >= romanNumerals[i].value) {
+            res += romanNumerals[i].numeral;
+            num -= romanNumerals[i].value;
+        }
     }
-};
-
-
-const romanTable = {
-    'I': 1,
-    'V': 5,
-    'X': 10,
-    'L': 50,
-    'C': 100,
-    'D': 500,
-    'M': 1000
+    return res;
 };
 
 const numConvert = () => {
-   let res = 0;
-   const toUpper = getInput.value.toUpperCase();
-    for (let i = 0; i < toUpper.length ; i++) {
-        const actualRoman = toUpper[i];
-        const actualValue = romanTable[actualRoman];
-        const nextValue = romanTable[toUpper + 1];
-        
-//         if () {
+    const inputValue = parseInt(getInput.value, 10);
 
-//         }
-//     }
-
-//    getOutput.innerText = `${res}`
+    if (getInput.value === '') {
+        getOutput.innerText = "Please enter a valid number";
+    } else if (inputValue < 1) {
+        getOutput.innerText = "Please enter a number greater than or equal to 1";
+    } else if (inputValue > 3999) {
+        getOutput.innerText = "Please enter a number less than or equal to 3999";
+    } else {
+        const romanres = numToRoman(inputValue);
+        getOutput.innerText = romanres;
     }
-}
+};
 
-convertBtn.addEventListener('click', numCheck);
 convertBtn.addEventListener('click', numConvert);
